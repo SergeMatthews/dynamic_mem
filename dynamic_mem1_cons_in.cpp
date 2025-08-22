5ba12b12c70381d04c88c1e01a647c833f15ca09
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-22 10:34:59 +0600

    Add files via upload

---
+++ dynamic_mem1_cons_in.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	int arr_size = 0;
+	cout << "Введите размер массива: ";
+	cin >> arr_size;
+
+	int* arr = new int[arr_size];
+
+	for (int i = 0; i < arr_size; ++i)
+	{
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+
+	cout << "Введённый массив: ";
+	for (int i = 0; i < arr_size; ++i)
+	{
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+
+	delete[] arr;
+	return 0;
+}
+
+
