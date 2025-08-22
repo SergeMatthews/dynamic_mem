c5615c187806f5e80448fdc6304ebe12a3eac5bd
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-22 10:37:43 +0600

    Add files via upload

---
+++ dynamic_mem2_create_arr.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+double* create_array(int size);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int arr_size = 0;
+    cout << "Введите размер массива: ";
+    cin >> arr_size;
+
+    double* arr = create_array(arr_size);
+    cout << "Массив: ";
+    for (int i = 0; i < arr_size; ++i)
+    {
+        cout << arr[i] << ' ';
+    }
+
+    delete[] arr; 
+    return 0;
+}
+
+double* create_array(int size) {
+    return new double[size] {};
+}
+
+
